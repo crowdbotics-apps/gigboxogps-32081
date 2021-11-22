2d97834f6ab355ee6f47481202aaccdb23fddf7f
Crowdbotics <team@crowdbotics.com>
2021-11-22 07:21:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "gigboxogps_32081",
+  url: "https://gigboxogps_32081.botics.co",
+  api: "https://gigboxogps_32081.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
